a9232bbf51d1efde6f70f204eb7adda46923023f
Max Lamb <maxlambcodes@gmail.com>
2020-07-28 17:40:55 -0700

    a little display stuff

--- src/components/CharacterItem.jsx
+++ src/components/CharacterItem.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CharacterItem = ({ name, species, status }) => {
+const CharacterItem = ({ name, species, status, image }) => {
   return (
-    <div>{name},{species},{status}</div>
+    <section>
+      <br></br>
+      <div>Name: {name}</div>
+      <div>Species: {species}</div>
+      <img src={image}></img>
+      <div>Status: {status}</div>
+      <br></br>
+    </section>
   );
 };
 
 CharacterItem.propTypes = {
   name: PropTypes.string.isRequired,
   species: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired
+  status: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
 };
 
 export default CharacterItem;
